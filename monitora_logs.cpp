9041d4ce8e7cab3604b0db52c3878799e12976a3
Enzo Cardoso Martins <enzocm.26@gmail.com>
2026-05-20 11:02:30 -0300

    Implementacao: Leitura de log simples

--- monitora_logs.cpp
+++ monitora_logs.cpp
@@ -1,9 +1,27 @@
 #include "monitora_logs.hpp"
 #include <bits/stdc++.h>
 #include <fstream>
+#include <filesystem>
 using namespace std;
+namespace fs = std::filesystem;
 
 string ler_log(string arq_name){
 
-    return "";
+    fs::path caminho = "./logs";
+    fs::path log_path = caminho / arq_name;
+
+
+    ifstream log(log_path);
+    string linha;
+
+    if(log.is_open()){
+
+        getline(log, linha);
+        log.close();
+        return linha;
+
+    } else{
+        return "Erro ao abrir o arquivo";
+    }
+
 }

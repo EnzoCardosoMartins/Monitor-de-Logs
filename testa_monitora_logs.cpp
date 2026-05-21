39ba07a832e8e7d074e85a2416e15d989729ceb2
Enzo Cardoso Martins <enzocm.26@gmail.com>
2026-05-21 11:24:38 -0300

    Teste: Formatacao de Tamanho

--- testa_monitora_logs.cpp
+++ testa_monitora_logs.cpp
@@ -65,3 +65,8 @@ TEST(MonitorLog, FormatacaoDeHora) {
     vector<string> logs = {"19/10/2006 10:07:23 TesteData3"};
     EXPECT_EQ(ler_arq(escrever_log_total("./logs/log_teste_hora.txt")), logs);
 }
+
+TEST(MonitorLog, FormatacaoTamanho) {
+    vector<string> logs = {"19/10/2006 10:07:23 TesteTamanho2"};
+    EXPECT_EQ(ler_arq(escrever_log_total("./logs/log_teste_tamanho.txt")), logs);
+}

4994a394a50100223cec5723f45b095e1ea3063c
Enzo Cardoso Martins <enzocm.26@gmail.com>
2026-05-21 19:53:55 -0300

    Teste: Ordenacao de Logs

--- monitora_logs.hpp
+++ monitora_logs.hpp
@@ -9,5 +9,6 @@ namespace fs = std::filesystem;
 string escrever_log_total(string log);
 vector<string> ler_arq(string arq_path);
 int processar_lista_mestra(string list_path);
+vector<string> sort_logs(vector<string>);
 
 #endif  // LOGMONITOR_HPP_

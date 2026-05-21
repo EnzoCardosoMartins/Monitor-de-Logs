1926cc9abfcca0cf15de432d44640e98ecd1ef01
Enzo Cardoso Martins <enzocm.26@gmail.com>
2026-05-20 22:17:09 -0300

    Implementacao: Escrever em um arquivo inexistente

--- monitora_logs.cpp
+++ monitora_logs.cpp
@@ -34,7 +34,11 @@ vector<string> ler_arq(string arq_path){
 
 string escrever_log_total(string log){
 
-    ofstream arq("./totais/total_log_teste.txt");
+    fs::path log_path = log;
+    string str_log_path = log_path.filename().string();
+    string string_total_path = ("./totais/total_");
+    fs::path total_path = string_total_path+str_log_path;
+    ofstream arq(total_path);
 
     if(arq.is_open()){
         vector<string> logs = ler_arq(log);
@@ -46,5 +50,6 @@ string escrever_log_total(string log){
     }
 
     arq.close();
-    return "./totais/total_log_teste.txt";
+    string str_arq_path = total_path.string();
+    return str_arq_path;
 }

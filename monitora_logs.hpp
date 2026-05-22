caa72205cadae491c9bd149df5beefc332c9963a
Enzo Cardoso Martins <enzocm.26@gmail.com>
2026-05-21 21:39:25 -0300

    Arrumando

--- monitora_logs.hpp
+++ monitora_logs.hpp
@@ -12,6 +12,5 @@ string escrever_log_total(string log);
 vector<string> ler_arq(string arq_path);
 int processar_lista_mestra(string list_path);
 vector<string> ler_logs(string log_path);
-vector<string> sort_logs(vector<string>);
 
 #endif  // MONITORA_LOGS_HPP_
